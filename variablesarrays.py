72b1eeac83aa6741ec09e5988e573ca7157d044f
Joyce Martinez <66332712+JoyceMT@users.noreply.github.com>
2021-03-09 16:40:32 -0600

    Add files via upload
    Variables and arrays

---
+++ variablesarrays.py
@@ -0,0 +1,48 @@
+#!C:/Users/"Username"/AppData/Local/Programs/Python/Python37-32/python.exe
+
+print 'Content-type: text/html'
+print ''
+
+age = 35
+
+print age
+
+pi = 3.14
+
+print pi
+
+print age / pi
+
+number = "5"
+
+print int(number) * pi
+
+str = "My name is Rob"
+
+print str[0]
+print str[0:5]
+
+print str + name
+
+myList = ["Rob", "Kirsten", "Ralphie"]
+
+print myList[1]
+
+myTuple = (1, 2, 3, 4)
+
+print myTuple
+
+print myTuple[2] = 5
+
+print myTuple
+
+dict = {}
+dict["dad"] = "Rob"
+dict["mom"] = "Kirsten"
+dict["brother"] = "Ralphie"
+
+print dict
+print dict["dad"]
+
+print dict.keys()
+print dict.values()
